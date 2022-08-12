370fadaca31c99575d828348d3afbe23f41a2d20
young13kkk <111087415+young13kkk@users.noreply.github.com>
2022-08-12 12:37:46 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+#Prints, strings y variables 
+
+print("hello, world")
+
+myint = 1
+print(myint)
+
+myfloat = 2
+print(myfloat)
+
+myene = 3
+print(myene)
+
+mystring = 'Young'
+print(mystring)
+mystring = '1s b4ck'
+print(mystring)
+mystring = '1n py'
+print(mystring)
+
+one = 1 
+two = 2
+three = one + two
+print(three)
+
+a, b = 13, 17
+print(a, b)
+
+hello = 'hello'
+a = '-young-'
+b = 'and-'
+c = 'Tempo'
+
+print(hello + a + b + c)
+
+#Lista
+
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
